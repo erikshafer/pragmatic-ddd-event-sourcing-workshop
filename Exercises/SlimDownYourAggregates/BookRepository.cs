645c62214a737c40b20af4a3ad28389d2cb84d5c
Erik Shafer <erik.shafer@gmail.com>
2023-07-19 05:21:28 -0500

    INCOMPLETE custom repository EXAMPLE

---
+++ Exercises/SlimDownYourAggregates/BookRepository.cs
@@ -0,0 +1,123 @@
+﻿using System.Text.Json;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using SlimDownYourAggregates.Slimmed;
+using SlimDownYourAggregates.Slimmed.Entities;
+
+namespace SlimDownYourAggregates;
+
+public interface IBookRepository
+{
+    ValueTask<BookModel> Find(BookId id);
+    void Store(BookEvent @event);
+    Task Save();
+}
+
+public interface IAmFancyOrm
+{
+    ValueTask<T> Find<T>(object id);
+    void Store<T>(T model);
+    Task Save();
+}
+
+public class ORMBookRepository: IBookRepository
+{
+    private readonly IAmFancyOrm orm;
+
+    public ORMBookRepository(IAmFancyOrm orm)
+    {
+        this.orm = orm;
+    }
+
+    public async ValueTask<Book> Find(BookId id)
+    {
+        var model = await orm.Find<BookModel>(id.Value);
+
+        var aggregate = (Book)(object)model;
+
+        return aggregate!;
+    }
+
+    public void Store(BookEvent @event)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Store(BookModel model, BookEvent @event)
+    {
+        orm.Store(model);
+        orm.Store(new Message(JsonSerializer.Serialize(@event)));
+    }
+
+    ValueTask<BookModel> IBookRepository.Find(BookId id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Save()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class ApplicationService
+{
+    private readonly IBookRepository repository;
+
+    public ApplicationService(IBookRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public async ValueTask HandleAdd(BookId id)
+    {
+        var aggregate = new Book(
+            new BookId(model.Id),
+            model.Title,
+            model.Author,
+            model.Genre,
+            model.Reviewers,
+            model.ISBN
+        );
+
+        //aggregate.AddChapter();
+
+        repository.Store(model);
+        await repository.Save();
+    }
+
+    public async ValueTask AddChapter(BookId id)
+    {
+        var model = await repository.Find(id);
+
+        var aggregate = new Book(
+            new BookId(model.Id),
+            model.Title,
+            model.Author,
+            model.Genre,
+            model.Reviewers,
+            model.ISBN
+        );
+
+        var @event = aggregate.AddChapter();
+
+        repository.Store(Evolve(model, @event));
+        await repository.Save();
+    }
+
+    public static BookModel Evolve(BookModel mongo, BookEvent @event)
+    {
+        switch (@event)
+        {
+            // case BookEvent.WritingStarted writingStarted:
+            //     return new BookModel(...);
+
+           case BookEvent.ChapterAdded chapterAdded:
+               bookModel.Chapters.Add(chapterAdded.Chapter);
+               return bookModel;
+
+           case BookEvent.Published published:
+               bookModel.CurrentState = BookModel.State.Published;
+               return bookModel;
+        }
+    }
+}
