b406760f9b84e147cd1f7cec3b47496b5ebd3f88
Erik Shafer <erik.shafer@gmail.com>
2023-07-19 12:57:52 -0500

    Re-revert

---
+++ Exercises/SlimDownYourAggregates/Slimmed/BookEvent.cs
@@ -0,0 +1,62 @@
+﻿using SlimDownYourAggregates.Slimmed.Entities;
+
+namespace SlimDownYourAggregates.Slimmed;
+
+public abstract record BookEvent
+{
+    public record WritingStarted(
+        BookId BookId,
+        Genre Genre,
+        Title Title,
+        Author Author,
+        Isbn Isbn
+    ) : BookEvent;
+
+    public record ChapterAdded(
+        BookId BookId,
+        Chapter Chapter
+    ) : BookEvent;
+
+    public record FormatAdded(
+        BookId BookId,
+        Format Format
+    ) : BookEvent;
+
+    public record FormatRemoved(
+        BookId BookId,
+        Format Format
+    ) : BookEvent;
+
+    public record TranslationAdded(
+        BookId BookId,
+        Translation Translation
+    ) : BookEvent;
+
+    public record MovedToEditing(
+        BookId BookId
+    ) : BookEvent;
+
+    public record Approved(
+        BookId BookId,
+        CommitteeApproval CommitteeApproval
+    ) : BookEvent;
+
+    public record MovedToPrinting
+    (
+        BookId BookId
+    ) : BookEvent;
+
+    public record Published(
+        BookId BookId,
+        Isbn Isbn,
+        Title Title,
+        Author Author
+    ) : BookEvent;
+
+    public record MovedToOutOfPrint
+    (
+        BookId BookId
+    ) : BookEvent;
+
+    private BookEvent() { }
+}
